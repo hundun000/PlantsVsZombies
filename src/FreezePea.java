bee8e46231c157a48e67e4588a958b16794b6690
lsj <lsj@DESKTOP-NCI4SRL>
2020-05-25 10:37:41 +0900

    FreezePea, Pea's advance() is changed and added attackZombie()

--- src/FreezePea.java
+++ src/FreezePea.java
@@ -15,24 +15,16 @@ public class FreezePea extends Pea {
         for (int i = 0; i < gp.getLaneZombies().get(getMyLane()).size(); i++) {
             Zombie z = gp.getLaneZombies().get(getMyLane()).get(i);
             Rectangle zRect = new Rectangle(z.getPosX(), 109 + getMyLane() * 120, 400, 120);
-            if (pRect.intersects(zRect)) {
-                z.setHealth(z.getHealth() - 300);
-                z.slow();
-                boolean exit = false;
-                if (z.getHealth() < 0) {
-                    System.out.println("ZOMBIE DIE");
-                    GamePanel.setProgress(10);
-                    gp.getLaneZombies().get(getMyLane()).remove(i);
-                    exit = true;
-                }
-                gp.getLanePeas().get(getMyLane()).remove(this);
-                if (exit) break;
-            }
+            
+            /* edited */
+            if(attackZombie(z, pRect,zRect,i)) break;
         }
-        /*if(posX > 2000){
-            gp.lanePeas.get(myLane).remove(this);
-        }*/
+        
         setPosX(getPosX() - 15);
     }
+    
+    
 
 }
+
+
